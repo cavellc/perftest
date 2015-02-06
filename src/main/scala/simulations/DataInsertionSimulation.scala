d01c3dbe19581b0b7ded2488cdadca80bc979ab1
Val Dumitrescu <vdumitrescu@gilt.com>
2015-02-06 11:34:35 +0000

    Expanded the Data Insertion simulation

--- src/main/scala/simulations/DataInsertionSimulation.scala
+++ src/main/scala/simulations/DataInsertionSimulation.scala
@@ -12,38 +12,44 @@ import scala.util.Random
 
 class DataInsertionSimulation extends io.gatling.core.scenario.Simulation {
 
-  val teams = TeamFeeder.teams.random
-  val metrics = MetricFeeder.metricDefinitions.random
-  val repeats = 10
+  SimulationConfiguration.init("insertion.conf") match {
+    case Some(config) =>
 
-  val scn = scenario("BasicSimulation")
-    .feed(teams)
-    .feed(metrics)
-    .repeat(repeats) {
-    exec { session =>
-      val metricName = session("metricName").as[String]
-      val metricTags = session("metricTags").as[Map[String, String]]
-      val timestamp = new DateTime(DateTimeZone.UTC).getMillis / 1000
+      val teams = TeamFeeder.teams.random
+      val metrics = MetricFeeder.metricDefinitions.random
 
-      val requestBody = Json.obj("metrics" -> Json.toJson(Seq(Metric(metricName, metricTags, timestamp, Random.nextDouble)))).toString
+      val scn = scenario("BasicSimulation")
+        .feed(teams)
+        .feed(metrics)
+        .repeat(config.repeats) {
+        exec { session =>
+          val metricName = session("metricName").as[String]
+          val metricTags = session("metricTags").as[Map[String, String]]
+          val timestamp = new DateTime(DateTimeZone.UTC).getMillis / 1000
 
-      session.setAll(
-        "requestUrl" -> TeamFeeder.teamUrl(session("teamName").as[String]),
-        "requestBody" -> requestBody
-      )
-    }
-    .exec {
-      http("name")
-        .post("${requestUrl}")
-        .header("Authorization", "Bearer ${teamToken}")
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .body(StringBody(stringToExpression[String]("${requestBody}")))
-    }
-    .pause(60.seconds)
+          val requestBody = Json.obj("metrics" -> Json.toJson(Seq(Metric(metricName, metricTags, timestamp, Random.nextDouble())))).toString()
+
+          session.setAll(
+            "requestUrl" -> TeamFeeder.teamUrl(session("teamName").as[String]),
+            "requestBody" -> requestBody
+          )
+        }
+        .exec {
+          http("name")
+            .post("${requestUrl}")
+            .header("Authorization", "Bearer ${teamToken}")
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .body(StringBody(stringToExpression[String]("${requestBody}")))
+        }
+        .pause(60.seconds)
+      }
+
+      setUp(scn.inject(
+        splitUsers(config.totalUsers) into atOnceUsers(config.atOnceUsers) separatedBy config.separatedBySeconds.seconds
+      ))
+
+    case None =>
+      println("Missing configuration for simulation.")
   }
-
-  setUp(scn.inject(
-    splitUsers(100) into (atOnceUsers(10)) separatedBy(6.seconds)
-  ))
 }
